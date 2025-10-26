b23848f9ab1198db38ffff86c2ddb16e47748d25
yeruslan72-svg <yeruslan72@gmail.com>
2025-10-26 10:16:09 +0300

    Create __init__.py

---
+++ plc_integration/plc_integration/plc_integration/__init__.py
@@ -0,0 +1,8 @@
+# PLC Integration Modules
+from .system_integrator import SoulPossessionIntegrator
+from .industrial_plc import AVCS_Soul_Integration
+
+__all__ = [
+    'SoulPossessionIntegrator',
+    'AVCS_Soul_Integration'
+]
